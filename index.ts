a77cc2e186d7819d235ffd7d680504e3a5f74492
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-13 10:17:34 +0545

    TS

---
+++ index.ts
@@ -0,0 +1,12 @@
+let fname: string ="Puja";
+fname="Sharma";
+
+let num:number;
+num=1;
+
+const names: string[]=[];
+names.push("PP")
+
+function sum (a:number, b:number):number{
+    return a+b;
+}
